e8ad8e92764b0eb9bdf1f6ed83fb97c71f2d1ee6
Adith A Danthi <adith.danthi@gmail.com>
2022-03-24 15:22:34 +0530

    feat: add products page

---
+++ src/views/Products/Products.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Sidebar, ProductCard } from '../../components';
+import { useProducts } from '../../contexts/product-context';
+import { getFilteredProducts } from './utils';
+import './Products.css';
+
+export function Products() {
+  const { products, filtersState } = useProducts();
+
+  const filteredProducts = getFilteredProducts(products, filtersState);
+
+  return (
+    <>
+      <Navbar />
+      <main className="shop-layout">
+        <Sidebar />
+        {/* Products List */}
+        <div className="products-grid">
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
