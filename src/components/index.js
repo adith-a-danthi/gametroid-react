35d88d3c08ea9158e03778977ff16108cf9ec372
Adith A Danthi <adith.danthi@gmail.com>
2022-03-26 18:53:50 +0530

    feat: add wishlist item

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as ProductCard } from './ProductCard/ProductCard';
 export { default as CartItem } from './CartItem/CartItem';
 export { default as CartSummary } from './CartSummary';
+export { default as WishlistItem } from './WishlistItem/WishlistItem';
