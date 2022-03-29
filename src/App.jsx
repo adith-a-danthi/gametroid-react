e2e740a134da6713819f3fb01d5473d4d2f6beb9
Adith A Danthi <adith.danthi@gmail.com>
2022-03-29 22:10:51 +0530

    chore: minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router';
 import { Home, Products, Cart, Wishlist, Login, Register } from './views';
 
+import './styles/base.css';
+
 function App() {
   return (
     <div className="App">
