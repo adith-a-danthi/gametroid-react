6b10f5d58da18c451a7b264e58469f8e2e34cb66
Adith A Danthi <adith.danthi@gmail.com>
2022-04-30 21:03:53 +0530

    feat: add 404 page

--- src/views/index.js
+++ src/views/index.js
@@ -4,3 +4,4 @@ export * from './Cart/Cart.jsx';
 export * from './Wishlist/Wishlist.jsx';
 export { default as Login } from './Login.jsx';
 export { default as Register } from './Register.jsx';
+export { default as Page404 } from './Page404.jsx';
