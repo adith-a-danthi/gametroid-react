6996b4745907ff9f3fe0ea4636b618d82e49356d
Adith A Danthi <adith.danthi@gmail.com>
2022-05-09 00:42:36 +0530

    feat: add cart API calls

---
+++ src/utils/cart-utils.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const getCartAPI = async (dispatchCart) => {
+  console.log('getCartAPI');
+  try {
+    const response = await axios.get('/api/user/cart', {
+      headers: { authorization: localStorage.getItem('token') },
+    });
+    console.log('Response: ', response.data);
+    dispatchCart({ type: 'UPDATE_CART_FROM_API', payload: response.data.cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addToCartAPI = async (dispatchCart, product) => {
+  try {
+    const response = await axios.post(
+      '/api/user/cart',
+      { product },
+      { headers: { authorization: localStorage.getItem('token') } }
+    );
+    dispatchCart({ type: 'UPDATE_CART_FROM_API', payload: response.data.cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeFromCartAPI = async (dispatchCart, product) => {
+  try {
+    const response = await axios.delete(`/api/user/cart/${product._id}`, {
+      headers: { authorization: localStorage.getItem('token') },
+    });
+    dispatchCart({ type: 'UPDATE_CART_FROM_API', payload: response.data.cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateQuantityAPI = async (dispatchCart, product, type) => {
+  console.log('updateQuantityAPI');
+  try {
+    const params = { action: { type } };
+    const response = await axios.post(`/api/user/cart/${product._id}`, params, {
+      headers: { authorization: localStorage.getItem('token') },
+    });
+    dispatchCart({ type: 'UPDATE_CART_FROM_API', payload: response.data.cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getCartAPI, addToCartAPI, removeFromCartAPI, updateQuantityAPI };
