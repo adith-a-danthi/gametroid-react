e25bf9bb91d0f4c65ad5c2640d7031188d514f6e
Adith A Danthi <adith.danthi@gmail.com>
2022-09-09 22:13:28 +0530

    chore: remove product context

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './styles/_reset.css';
 import App from './App';
 import { makeServer } from './server';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ProductProvider } from './contexts/product-context';
 import { CartProvider } from './contexts/cart-context';
 import { WishlistProvider } from './contexts/wishlist-context';
 import { AuthProvider } from './contexts/auth-context';
@@ -18,15 +17,13 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <ProductProvider>
-          <AuthProvider>
-            <CartProvider>
-              <WishlistProvider>
-                <App />
-              </WishlistProvider>
-            </CartProvider>
-          </AuthProvider>
-        </ProductProvider>
+        <AuthProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <App />
+            </WishlistProvider>
+          </CartProvider>
+        </AuthProvider>
       </Provider>
     </Router>
   </React.StrictMode>,
